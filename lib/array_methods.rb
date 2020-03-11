e5ce8eac1fd877e826c52d9a82bdec389d49954c
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-11 02:56:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,7 @@ end
 
 def find_max_value(array)
   index = 0
-  max = 0
+  max = array[0]
   while array[index] do 
     if max < array[index]
       max = array[index]
@@ -20,7 +20,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min = 0
+  min = array[0]
   array.length.times do |index| 
     if array[index] < min 
       min = array[index]
